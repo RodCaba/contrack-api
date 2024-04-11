74520cd4facadd5f30b2efc58f19100bc52f216b
RodCaba <rodser4@gmail.com>
2024-04-10 21:04:19 -0700

    Refactor router

--- router/project.go
+++ router/project.go
@@ -2,22 +2,14 @@ package router
 
 import (
 	"contrack-api/controllers"
-
 	"github.com/gin-gonic/gin"
 )
 
-func Init() *gin.Engine {
-	router := gin.New()
-
-	router.Use(gin.Logger())
-	router.Use(gin.Recovery())
-
+func InitProjectRouter(router *gin.Engine) {
 	{
 		project := router.Group("/projects")
 		project.GET("", controllers.FindProjects)
 		project.GET("/:id", controllers.FindProject)
 		project.POST("", controllers.CreateProject)
 	}
-
-	return router
 }
---
+++ router/router.go
@@ -0,0 +1,17 @@
+package router
+
+import (
+
+	"github.com/gin-gonic/gin"
+)
+
+func Init() *gin.Engine {
+	router := gin.New()
+
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	InitProjectRouter(router)
+
+	return router
+}
