9a87271faee409963584f95911ccaac3c777ad82
RodCaba <rodser4@gmail.com>
2024-04-10 21:30:59 -0700

    Change name struct project #

--- models/project.go
+++ models/project.go
@@ -3,7 +3,7 @@ package models
 type Project struct {
 	ID uint `json:"id" gorm:"primary_key"`
 	AgreedDeliveryDate string `json:"agreedDeliveryDate"`
-	LastEstimate float64 `json:"lastEstimate"`
+	LastEstimatedIncrease float64 `json:"lastEstimatedIncrease"`
 	LastEstimateDate string `json:"lastEstimateDate"`
 	AgreedPrice float64 `json:"agreedPrice"`
 	OverallProgress float64 `json:"overallProgress"`
